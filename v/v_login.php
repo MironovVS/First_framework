6d6a68df2b261ccdabfacfe95bd8baae11879364
MironovVS <MAdd2062@gmail.com>
2015-12-16 01:16:01 +0400

    Страница ввода логина и пароля

---
+++ v/v_login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+  <title>Веб-Гуру</title>
+  <meta content="text/html; charset=utf-8" http-equiv="content-type">
+  <link rel="stylesheet" type="text/css" media="screen" href="theme/style.css" />
+</head>
+<body>
+<h1>Авторизация</h1>
+<a href="index.php">Главная</a> | <a href="reg.php">Зарегистрироваться</a>
+
+<form method="post">
+  E-mail: <input type="text" name="login" /><br/>
+  Пароль: <input type="password" name="password" /><br/>
+  <input type="checkbox" name="remember" /> Запомить меня<br/>
+  <input type="submit" />
+</form>
+</body>
+</html>
